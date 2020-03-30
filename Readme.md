5430639f1731d4b61c0dd1788680aeb3ff97d1a8
Elliot Marsden <elliot.marsden@habito.com>
2020-03-30 13:44:38 +0200

    First commit

---
+++ Readme.md
@@ -0,0 +1,89 @@
+# Surround: A package for Sublime Text 3
+
+Change or delete the characters surrounding your selection.
+
+## Intent
+
+To do things like:
+
+- Swap `"` quotes for `'`
+- Remove unused parantheses
+
+Those are the main uses I can think of, but they're pretty common.
+
+## Installation
+
+### Recommended option: Use the Package Control plugin
+
+- Follow [these instructions](http://wbond.net/sublime_packages/package_control) to install the Package Control plugin
+- In Sublime Text, pick `Package Control: Install Package` from the command palette, then search for and select the package `Surround`.
+
+### Alternative option: Clone the repository
+
+Clone the repository containing the plugin into `Sublime Text 3/Packages`, whose location depends on your operating system:
+
+MacOS:
+
+`git clone git://github.com/eddiejessup/Surround.git ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/Surround`
+
+Windows:
+
+`git clone git://github.com/eddiejessup/Surround.git "%APPDATA%\Sublime Text 3\Packages\Surround"`
+
+Linux:
+
+`git clone git://github.com/eddiejessup/Surround.git ~/.config/sublime-text-3/Packages/Surround`
+
+## Usage
+
+TODO: Test and Document 'add surroundings'
+
+### Low-level functionality
+
+The package provides the command, `surround_change`. This asks for two inputs: the character to search for (the 'match'), and the character to swap it with (the 'replacement'). It will then look up a pair of characters for the match character, and look for the nearest enclosing pair of those characters, and swap them with the pair of characters represented by the replacement character.
+
+There is also the `surround_delete` command, that only asks for a match character, and which simply removes the corresponding nearest pair.
+
+### Command-palette interface
+
+The plugin adds entries to the command palette called `Surround: {Change,Delete} surroundings`.
+
+### Menu interface
+
+The plugin adds menu entries under `Selection/{Change,Delete} surroundings`.
+
+### Key-binding interface
+
+The plugin binds a shortcut, `"ctrl+k", "ctrl+c"` to run the `surround_change` command, or `"ctrl+k", "ctrl+shift+c"` to run the `surround_delete` command. On MacOS, the "super" (command) key is bound instead of the "ctrl" key.
+
+You can also define a keyboard shortcut which runs the commands with particular arguments, by binding a key pattern to the `surround_change_text` command with the arguments `match` and `replacement` specifying your intended find/replace characters.
+
+For example, to add shortcuts to replace `'` with `"` and vice versa, you might add the key bindings:
+
+```
+{
+    "keys": ["super+k", "super+'"],
+    "command": "surround_change_text",
+    "args": { "match": "\"", "replacement": "'" }
+},
+{
+    "keys": ["super+k", "super+shift+'"],
+    "command": "surround_change_text",
+    "args": { "match": "'", "replacement": "\"" }
+}
+```
+
+## Configuration
+
+You can edit the pair mappings, and plugin settings through the menu, at `Sublime Text/Preferences/Package Settings/Surround`.
+
+## Author & Contributors
+
+This is an updated and tweaked version of a plugin by [jcartledge](https://github.com/jcartledge), so thanks to them. I was too lazy to properly contribute to that plugin, so I forked it, sorry.
+
+- [Elliot Marsden](https://github.com/eddiejessup)
+- [jcartledge](https://github.com/jcartledge/sublime-surround)
+
+## License
+
+MIT License
---
+++ Surround.py
@@ -0,0 +1,174 @@
+"""
+@name     Surround
+@package  sublime_plugin
+@author   Elliot Marsden
+
+Change or delete characters surrounding a selection.
+"""
+# pylint: disable=too-few-public-methods
+
+import re
+
+import sublime  # pylint: disable=import-error
+import sublime_plugin  # pylint: disable=import-error
+
+TAG_REGEX = r"<([\S]+)([^>]*)>"
+
+def settings():
+    return sublime.load_settings("Surround.sublime-settings")
+
+def pair_from_char(surround_char, pairs):
+    return pairs.get(surround_char, [(surround_char), surround_char])
+
+def search_patterns_for_surround(surround_char):
+    surround_pair = taggify_for_search(
+        pair_from_char(
+            surround_char,
+            settings()["pairs_for_search"],
+        )
+    )
+    return dict(
+        open=surround_pair[0],
+        close=surround_pair[1],
+        flags=sublime.LITERAL if len(surround_pair[0]) <= 1 else 0
+    )
+
+def pair_replacement_char(surround_char):
+    return taggify_for_replacement(
+        pair_from_char(
+            surround_char,
+            settings()["pairs_for_replace"],
+        )
+    )
+
+def taggify_for_replacement(surround_pair):
+    matches = re.search(TAG_REGEX, surround_pair[0])
+    if matches:
+        return [surround_pair[0], "</" + matches.group(1) + ">"]
+    return surround_pair
+
+def taggify_for_search(surround_pair):
+    matches = re.search(TAG_REGEX, surround_pair[0])
+    if matches:
+        attrs = matches.group(2)
+        if len(attrs) == 0:
+            attrs = r"([\s]+[^>]*)?"
+        open_tag = str("<" + matches.group(1) + attrs + ">")
+        close_tag = str("</" + matches.group(1) + ">")
+        return [open_tag, close_tag]
+    return surround_pair
+
+# Add surroundings.
+
+class SurroundSelectionWindowCommand(sublime_plugin.WindowCommand):
+    """ Surround the selection with something. """
+
+    def run(self):
+        self.window.show_input_panel(
+            "Surround with:",
+            "",
+            lambda s: self.window.active_view().run_command(
+                "surround_selection",
+                dict(surround_char=s)
+            ),
+            None,
+            None,
+        )
+
+class SurroundSelectionCommand(sublime_plugin.TextCommand):
+    """ Surround the selection with something """
+
+    def run(self, edit, surround_char):
+        surround_pair = pair_replacement_char(surround_char)
+
+        for region in reversed(self.view.sel()):
+            self.view.insert(edit, region.end(), surround_pair[1])
+            self.view.insert(edit, region.begin(), surround_pair[0])
+
+# Change/delete surroundings.
+
+class SurroundChangeCommand(sublime_plugin.WindowCommand):
+    """ Change the selection's surroundings. """
+
+    def run(self):
+        def match_callback(match):
+            self.window.show_input_panel(
+                "Replace with:",
+                "",
+                lambda r: self.window.active_view().run_command(
+                    "surround_change_text",
+                    dict(match=match, replacement=r)
+                ),
+                None,
+                None,
+            )
+
+        self.window.show_input_panel(
+            "Match:",
+            "",
+            match_callback,
+            None,
+            None,
+        )
+
+class SurroundDeleteCommand(sublime_plugin.WindowCommand):
+    """ Delete something surrounding something. """
+
+    def run(self):
+        self.window.show_input_panel(
+            "Delete:",
+            "",
+            lambda m: self.window.active_view().run_command(
+                "surround_change_text",
+                dict(match=m, replacement='')
+            ),
+            None,
+            None,
+        )
+
+class SurroundChangeTextCommand(sublime_plugin.TextCommand):
+    """ Change the selection's surroundings. """
+
+    def run(self, edit, match, replacement):
+        search = search_patterns_for_surround(match)
+        replacement_pair = pair_replacement_char(replacement)
+
+        # Traverse regions backwards so edits don't clobber each other.
+        for region in reversed(self.view.sel()):
+            end = self.find_end(region.end(), search)
+            start = self.find_start(region.begin(), search)
+            self.view.replace(edit, end, replacement_pair[1])
+            self.view.replace(edit, start, replacement_pair[0])
+
+    def find_start(self, to_pos, search):
+        open_matches = self.find_between(0, to_pos, search['open'], search['flags'])
+        if not open_matches:
+            raise RuntimeError(f"Starting pair not found: {search['open']}")
+
+        prev = open_matches.pop()
+
+        # TODO: I don't think this balancing logic is correct.
+        # Balance pairs.
+        nr_closers = len(
+            self.find_between(prev.end(), to_pos, search['close'], search['flags'])
+        )
+        return prev if nr_closers % 2 == 0 else self.find_start(prev.begin(), search)
+
+    def find_end(self, from_pos, search):
+        nxt = self.view.find(search['close'], from_pos, search['flags'])
+        if nxt is None:
+            raise RuntimeError(f"Ending pair not found: {search['close']}")
+
+        # Balance pairs.
+        nr_openers = len(
+            self.find_between(from_pos, nxt.begin(), search['open'], search['flags'])
+        )
+        return nxt if nr_openers % 2 == 0 else self.find_end(nxt.end(), search)
+
+    def find_between(self, from_pos, to_pos, to_find, flags):
+        # TODO: Sounds inefficient, better API calls?
+        return [
+            find for find in self.view.find_all(to_find, flags)
+            if find.end() <= to_pos
+            and find.begin() >= from_pos
+        ]
